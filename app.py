c977929c5afb6aa2e1b1490d72207d329933e09b
Alphactory <aedan.e.taylor@gmail.com>
2023-08-11 22:13:49 -0400

    stupid typo, i hate autorefactor

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def resume():
-    return flask.send_file("Taylor_Aedan_AT_Resume.pdf.pdf")
+    return flask.send_file("Taylor_Aedan_AT_Resume.pdf")
 
 @app.route('/rickroll')
 def rickroll():
